2353533be7dcec3077a47d7af2579c617f2a52f4
lpbaiser <lpbaiser@gmail.com>
2016-04-13 11:49:19 -0300

    Classe ReservaDAO criada

---
+++ src/main/java/br/edu/utfpr/biblioteca/salas/dao/ReservaDAO.java
@@ -0,0 +1,15 @@
+package br.edu.utfpr.biblioteca.salas.dao;
+
+import br.edu.utfpr.biblioteca.salas.model.Reserva;
+
+/**
+ *
+ * @author leonardo
+ */
+public class ReservaDAO extends GenericDAO<Reserva> {
+
+    public ReservaDAO() {
+        super(Reserva.class);
+    }
+
+}
