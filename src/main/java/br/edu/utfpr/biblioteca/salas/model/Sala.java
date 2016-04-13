88e910451b451c7f6d13add4abfabd6f7e5ac412
MateusVT <torres@alunos.utfpr.edu.br>
2016-04-13 02:39:26 -0300

    Classes Estudante, Administrador e Sala, implementadas.

---
+++ src/main/java/br/edu/utfpr/biblioteca/salas/model/Sala.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.biblioteca.salas.model;
+
+/**
+ *
+ * @author RAZIEL
+ */
+public class Sala {
+
+    int id;
+    boolean ar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setPontos(int id) {
+        this.id = id;
+    }
+
+    public boolean getAr() {
+        return ar;
+    }
+
+    public void setAr(boolean ar) {
+        this.ar = ar;
+    }
+
+}
