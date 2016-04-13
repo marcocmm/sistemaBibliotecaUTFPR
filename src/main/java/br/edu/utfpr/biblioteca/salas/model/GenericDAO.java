3c8265367dd71105ea416f6b083ef3f29ba74ded
MateusVT <torres@alunos.utfpr.edu.br>
2016-04-13 00:52:43 -0300

    GenericDao implementado

---
+++ src/main/java/br/edu/utfpr/biblioteca/salas/model/GenericDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.biblioteca.salas.model;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author a1602063
+ */
+public abstract class GenericDAO<T> {
+
+    public static EntityManager entityManager = Persistence.createEntityManagerFactory("UP").createEntityManager();
+
+    private Class clazz;
+
+    public GenericDAO(Class clazz) {
+        this.clazz = clazz;
+    }
+
+    public void insert(T entity) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(entity);
+        entityManager.getTransaction().commit();
+    }
+
+    public void update(T entity) {
+        entityManager.getTransaction().begin();
+        entityManager.merge(entity);
+        entityManager.getTransaction().commit();
+    }
+
+    public void delete(T entity) {
+        if (entity != null) {
+            entityManager.getTransaction().begin();
+            entityManager.remove(entity);
+            entityManager.getTransaction().commit();
+        }
+    }
+
+    public T obter(int id) {
+        entityManager.clear();
+        return (T) entityManager.find(clazz, id);
+    }
+
+    public List<T> list() {
+        return entityManager.createQuery("SELECT e FROM " + clazz.getSimpleName() + " e").getResultList();
+    }
+
+}
