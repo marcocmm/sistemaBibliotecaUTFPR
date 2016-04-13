5201303be2c86d8a93110179d2688e39dda1606c
mateus <mateustomoo@gmail.com>
2016-04-12 21:10:24 -0300

    Classe Reserva implementada

--- src/main/java/br/edu/utfpr/biblioteca/salas/model/Reserva.java
+++ src/main/java/br/edu/utfpr/biblioteca/salas/model/Reserva.java
@@ -5,10 +5,77 @@
  */
 package br.edu.utfpr.biblioteca.salas.model;
 
-/**
- *
- * @author romulo
- */
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Reservas")
 public class Reserva {
+    @Column(name="id")
+    private int id;
+    @Column(name="quantidade_alunos")
+    private int quantidadeAlunos;
+    @Column(name="data_inicial")
+    private Date dataInicial;
+    @Column(name="data_final")
+    private Date dataFinal;
+    @Column(name="estudante_ra")
+    private Estudante estudanteRA;
+    @Column(name="sala_id")
+    private Sala sala;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantidadeAlunos() {
+        return quantidadeAlunos;
+    }
+
+    public void setQuantidadeAlunos(int quantidadeAlunos) {
+        this.quantidadeAlunos = quantidadeAlunos;
+    }
+
+    public Date getDataInicial() {
+        return dataInicial;
+    }
+
+    public void setDataInicial(Date dataInicial) {
+        this.dataInicial = dataInicial;
+    }
+
+    public Date getDataFinal() {
+        return dataFinal;
+    }
+
+    public void setDataFinal(Date dataFinal) {
+        this.dataFinal = dataFinal;
+    }
+
+    public Estudante getEstudanteRA() {
+        return estudanteRA;
+    }
+
+    public void setEstudanteRA(Estudante estudanteRA) {
+        this.estudanteRA = estudanteRA;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
     
 }
+
+
+
+    
