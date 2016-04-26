980ae474a19bc0873cd28687694639b3bd68aa2a
lpbaiser <lpbaiser@gmail.com>
2016-04-26 20:22:35 -0300

    Criado classe CalendarView.java e editado arquivo index.xhtml

---
+++ src/main/java/br/edu/utfpr/biblioteca/salas/view/CalendarView.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.utfpr.biblioteca.salas.view;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.SelectEvent;
+
+@ManagedBean
+public class CalendarView {
+
+    private Date date;
+
+    public void onDateSelect(SelectEvent event) {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Date Selected", format.format(event.getObject())));
+        
+        //teste
+        System.out.println("data: " + getDate1());
+    }
+
+    public void click() {
+        RequestContext requestContext = RequestContext.getCurrentInstance();
+
+        requestContext.update("form:display");
+        requestContext.execute("PF('dlg').show()");
+
+
+    }
+
+    public Date getDate1() {
+        return date;
+    }
+
+    public void setDate1(Date date1) {
+        this.date = date1;
+    }
+
+}
--- target/generated-sources/annotations/br/edu/utfpr/biblioteca/salas/model/Reserva_.java
+++ target/generated-sources/annotations/br/edu/utfpr/biblioteca/salas/model/Reserva_.java
@@ -1,21 +1,26 @@
 package br.edu.utfpr.biblioteca.salas.model;
 
 import br.edu.utfpr.biblioteca.salas.model.Estudante;
+import br.edu.utfpr.biblioteca.salas.model.ReservaAtiva;
 import br.edu.utfpr.biblioteca.salas.model.Sala;
 import java.util.Date;
 import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-14T21:55:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-22T23:57:46")
 @StaticMetamodel(Reserva.class)
 public class Reserva_ { 
 
+    public static volatile ListAttribute<Reserva, ReservaAtiva> reservasAtivasList;
     public static volatile SingularAttribute<Reserva, Integer> quantidadeAlunos;
-    public static volatile SingularAttribute<Reserva, Sala> sala;
+    public static volatile SingularAttribute<Reserva, Sala> salaId;
     public static volatile SingularAttribute<Reserva, Integer> id;
     public static volatile SingularAttribute<Reserva, Date> dataInicial;
+    public static volatile ListAttribute<Reserva, ReservaAtiva> reservasAtivasList2;
+    public static volatile ListAttribute<Reserva, ReservaAtiva> reservasAtivasList1;
     public static volatile SingularAttribute<Reserva, Date> dataFinal;
-    public static volatile SingularAttribute<Reserva, Estudante> estudanteRA;
+    public static volatile SingularAttribute<Reserva, Estudante> estudanteRa;
 
 }
