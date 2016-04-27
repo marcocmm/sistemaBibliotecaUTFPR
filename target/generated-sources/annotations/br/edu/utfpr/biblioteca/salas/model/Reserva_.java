f030a253ebb1b8239ac880dad4b50084a118a510
marcocmm <marco.cmm@gmail.com>
2016-04-26 21:02:11 -0300

    PrimeFaces com bootstrap

--- target/generated-sources/annotations/br/edu/utfpr/biblioteca/salas/model/Reserva_.java
+++ target/generated-sources/annotations/br/edu/utfpr/biblioteca/salas/model/Reserva_.java
@@ -9,18 +9,18 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-22T23:57:46")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-04-26T20:26:40")
 @StaticMetamodel(Reserva.class)
 public class Reserva_ { 
 
-    public static volatile ListAttribute<Reserva, ReservaAtiva> reservasAtivasList;
+    public static volatile SingularAttribute<Reserva, ReservaAtiva> reservaAtiva;
+    public static volatile ListAttribute<Reserva, Date> datasReservasAtivas;
     public static volatile SingularAttribute<Reserva, Integer> quantidadeAlunos;
-    public static volatile SingularAttribute<Reserva, Sala> salaId;
+    public static volatile SingularAttribute<Reserva, Sala> sala;
     public static volatile SingularAttribute<Reserva, Integer> id;
     public static volatile SingularAttribute<Reserva, Date> dataInicial;
-    public static volatile ListAttribute<Reserva, ReservaAtiva> reservasAtivasList2;
-    public static volatile ListAttribute<Reserva, ReservaAtiva> reservasAtivasList1;
+    public static volatile SingularAttribute<Reserva, Estudante> estudante;
     public static volatile SingularAttribute<Reserva, Date> dataFinal;
-    public static volatile SingularAttribute<Reserva, Estudante> estudanteRa;
+    public static volatile ListAttribute<Reserva, Estudante> estudantesReservasAtivas;
 
 }
