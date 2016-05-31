25dd985179f9e4eef075f1cc7d5d5cbdc1de0b7d
Rômulo Manciola Meloca <rmeloca@gmail.com>
2016-05-31 04:43:15 -0300

    REMOVENDO PRINTLNgit add .!

--- src/main/java/br/edu/utfpr/biblioteca/salas/controller/ReservaRapidaMB.java
+++ src/main/java/br/edu/utfpr/biblioteca/salas/controller/ReservaRapidaMB.java
@@ -108,7 +108,6 @@ public class ReservaRapidaMB implements Serializable {
         Date dataSelecionada = (Date) event.getObject();
         FacesContext facesContext = FacesContext.getCurrentInstance();
         facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "Data Selecionada", formatoEmDia.format(event.getObject())));
-        System.out.println(dataSelecionada);
         updateBotoesAtivosPorDia(dataSelecionada);
     }
 
